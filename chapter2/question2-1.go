85512054ccaee2c07fac9604310781831232784f
ryota548 <trb_right9@yahoo.co.jp>
2018-08-18 16:54:35 +0900

    modify question 2-1

--- chapter2/question2-1.go
+++ chapter2/question2-1.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"time"
 )
 
 func main() {
+	file, err := os.OpenFile("test.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		//エラー処理
+		log.Fatal(err)
+	}
+	defer file.Close()
 	rand.Seed(time.Now().UnixNano())
-	fmt.Fprintf(os.Stdout, "Write with os.Stdout at %d\n", rand.Intn(10))
-	fmt.Fprintf(os.Stdout, "Write with os.Stdout at %s\n", "Hello golang")
-	fmt.Fprintf(os.Stdout, "Write with os.Stdout at %f\n", rand.Float64()*10)
+	fmt.Fprintf(file, "Write with os.Stdout at %d\n", rand.Intn(10))
+	fmt.Fprintf(file, "Write with os.Stdout at %s\n", "Hello golang")
+	fmt.Fprintf(file, "Write with os.Stdout at %f\n", rand.Float64()*10)
 }
